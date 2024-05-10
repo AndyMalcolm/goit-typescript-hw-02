e92bb48fc9b640bdafd638900bb4cf8aa1953f2a
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2024-05-10 21:57:24 +0300

    good?

--- src/service/api.ts
+++ src/service/api.ts
@@ -19,20 +19,3 @@ export const getPhotos = async <T>(query: string, page = 1, per_page = 20): Prom
 
 
 
-// const instance = axios.create({
-//   baseURL: "https://api.unsplash.com",
-// });
-
-// const API_KEY = 'i9wbXvFd-cu6QxKyhooRASCj8VWy50rtQG-9BbauSY4';
-
-// instance.defaults.headers.common['Authorization'] = `Client-ID ${API_KEY}`;
-
-// export const getPhotos = async( query, page ) => {
-
-//         const {data} = await instance.get(`/search/photos?page=${page}&query=${query}&per_page=${20}`)
-
-//     return data;
-// } 
-
-
-
